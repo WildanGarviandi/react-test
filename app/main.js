c8c18233035bea9b814bfa4c2e405e6e7bc3c6bc
Cubicka <agung@etobee.com>
2016-03-28 15:20:47 +0700

    Add production build

--- app/main.js
+++ app/main.js
@@ -3,52 +3,9 @@ import fetch from 'isomorphic-fetch';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
-
-import checkAuth from './modules/auth/actions/checkAuth';
 import store from './store';
-import ContainerPage from './views/container';
-import ContainerDetailsPage from './views/container/details';
-import ContainerFillPage from './views/container/fill';
-import ContainerOrderPage from './views/container/order';
-import DashboardPage from './views/dashboard';
-import LoginPage from './views/login';
-import {HubPage, MyOrderPage} from './views/order';
 import './main.css';
-
-function App(props) {
-  return <div style={{height: "100%"}}>{props.children}</div>;
-}
-
-function requireAuth(nextState, replace, callback) {
-  checkAuth(store).then(function(result) {
-    if(!result.ok) {
-      replace({
-        pathname: '/login',
-        state: { nextPathname: nextState.location.pathname }
-      })
-    }
-
-    callback();
-  });
-}
-
-const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="/home" component={DashboardPage} onEnter={requireAuth}>
-        <IndexRoute component={ContainerPage} />
-        <Route path="/hubOrder" component={HubPage} />
-        <Route path="/container" component={ContainerPage} />
-        <Route path="/container/:id" component={ContainerDetailsPage} />
-        <Route path="/container/:id/order" component={ContainerOrderPage} />
-        <Route path="/container/:id/fill" component={ContainerFillPage} />
-      </Route>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/*" component={LoginPage} />
-    </Route>
-  </Router>
-);
+import routes from './routes';
 
 const Root = () => {
   return (
---
+++ app/routes.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Router, Route, IndexRoute, browserHistory, hashHistory, useRouterHistory } from 'react-router';
+import checkAuth from './modules/auth/actions/checkAuth';
+import ContainerPage from './views/container';
+import ContainerDetailsPage from './views/container/details';
+import ContainerFillPage from './views/container/fill';
+import ContainerOrderPage from './views/container/order';
+import DashboardPage from './views/dashboard';
+import LoginPage from './views/login';
+import {HubPage, MyOrderPage} from './views/order';
+import store from './store';
+
+import { createHashHistory } from 'history';
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
+function App(props) {
+  return <div style={{height: "100%"}}>{props.children}</div>;
+}
+
+function requireAuth(nextState, replace, callback) {
+  checkAuth(store).then(function(result) {
+    if(!result.ok) {
+      replace({
+        pathname: '/login',
+        state: { nextPathname: nextState.location.pathname }
+      })
+    }
+
+    callback();
+  });
+}
+
+export default (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={LoginPage} />
+      <Route path="/home" component={DashboardPage} onEnter={requireAuth}>
+        <IndexRoute component={ContainerPage} />
+        <Route path="/hubOrder" component={HubPage} />
+        <Route path="/container" component={ContainerPage} />
+        <Route path="/container/:id" component={ContainerDetailsPage} />
+        <Route path="/container/:id/order" component={ContainerOrderPage} />
+        <Route path="/container/:id/fill" component={ContainerFillPage} />
+      </Route>
+      <Route path="/login" component={LoginPage} />
+      <Route path="/*" component={LoginPage} />
+    </Route>
+  </Router>
+);
