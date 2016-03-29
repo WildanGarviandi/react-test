5a839f2bc4759317eb41ed324aee00828137f268
Cubicka <agung@etobee.com>
2016-03-29 18:20:23 +0700

    No changes. Testing for automatic pulling at hub.etobee.com

--- app/reducers.js
+++ app/reducers.js
@@ -4,7 +4,7 @@ import authReducers from './modules/auth/reducers';
 import containersReducers from './modules/containers/reducers';
 import districtsReducers from './modules/districts/reducers';
 
-export default combineReducers({
+export default combineReducers({ 
   userLogged: authReducers,
   containers: containersReducers.containers,
   ordersPrepared: containersReducers.ordersPrepared,
