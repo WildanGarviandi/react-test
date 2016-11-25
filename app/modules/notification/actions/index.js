c970d418154ab02692701e7ff66ee3725c53e5c4
MKImaduddin <mk.imaduddin@gmail.com>
2016-11-25 17:20:56 +0700

    refactor

--- app/modules/notification/actions/index.js
+++ app/modules/notification/actions/index.js
@@ -3,9 +3,9 @@ import {notifAction} from '../constants';
 function addNotification (message, level, position) {
   return {
     type: notifAction.ADD_NOTIFICATION,
-      message,
-      level,
-      position
+    message,
+    level,
+    position
   };
 }
 
