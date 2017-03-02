3c8f1e8d723e25abc6326f0351e39875aaa94e80
dekhangga <danggareska@yahoo.com>
2017-03-02 08:09:41 +0700

    hotfix authentication

--- app/modules/fetch/delete.js
+++ app/modules/fetch/delete.js
@@ -15,5 +15,10 @@ const DeleteParams = (token, body) => {
 
 export default (url, token, body = {}, isHubAPI = false) => {
   let baseUrl = isHubAPI ? config.baseUrlHub : config.baseUrl;
-  return fetch(baseUrl + url, DeleteParams(token, body));
+  return fetch(baseUrl + url, DeleteParams(token, body)).then(function(response) {
+    if (response.status === 403) {
+      window.location.href = '/login';
+    }
+    return response;
+  });
 }
