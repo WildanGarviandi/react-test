7cd1ff195f05662f06ad344494a548b7051b13c7
sujono <sujono@etobee.com>
2017-05-10 15:16:33 +0700

    Add Global ESLint Variable

--- app/modules/auth/reducers/userLogged.js
+++ app/modules/auth/reducers/userLogged.js
@@ -1,8 +1,8 @@
-import * as actionTypes from '../constants';
+import * as actionTypes from '../constants'; //eslint-disable-line
 
 const initialUserState = {
   isFetching: false,
-  isValid: true, 
+  isValid: true,
   user: {},
   token: localStorage.token,
   userID: localStorage.userID,
@@ -10,29 +10,29 @@ const initialUserState = {
   hubName: localStorage.hubName,
   fleetName: localStorage.fleetName,
   isCentralHub: false,
-  editCOD: false, 
+  editCOD: false,
   editVolume: false,
-  editWeight: false
+  editWeight: false,
 };
 
 export default (state = initialUserState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case actionTypes.LOGIN_START:
       localStorage.clear();
-      return _.assign({}, state, {isFetching: true, isValid: false});
+      return _.assign({}, state, { isFetching: true, isValid: false });
     case actionTypes.LOGIN_SUCCESS:
       localStorage.token = action.user.LoginSessionKey;
       localStorage.userID = action.user.UserID;
       return _.assign({}, state, {
-        isFetching: false, 
-        isValid: true, 
+        isFetching: false,
+        isValid: true,
         user: action.user,
         token: action.user.LoginSessionKey,
         userID: action.user.UserID
       });
     case actionTypes.LOGIN_FAILED:
       localStorage.clear();
-      return _.assign({}, state, {isFetching: false, isValid: false, message: action.message});
+      return _.assign({}, state, { isFetching: false, isValid: false, message: action.message });
     case actionTypes.AUTH_VALID:
       localStorage.hubID = action.hub && action.hub.HubID;
       localStorage.hubName = action.hub && action.hub.Name;
@@ -43,7 +43,7 @@ export default (state = initialUserState, action) => {
         hubName: action.hub && action.hub.Name,
         fleetName: action.user && action.user.User && action.user.User.CompanyDetail && action.user.User.CompanyDetail.CompanyName,
         isCentralHub: action.hub && ("CENTRAL" === action.hub.Type),
-        editCOD: action.order && action.order.edit_cod, 
+        editCOD: action.order && action.order.edit_cod,
         editVolume: action.order && action.order.edit_volume,
         editWeight: action.order && action.order.edit_weight
       });
