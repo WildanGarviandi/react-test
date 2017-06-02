2c70813ddddd949965d1bb382c8819944faa053a
sujono <sujono@etobee.com>
2017-06-02 17:42:34 +0700

    Upgrade Webpack and fix local indent name in css loader

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,58 +1,63 @@
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+/* eslint-disable */
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: __dirname + "/app/main.js",
+  entry: __dirname + '/app/main.js',
   output: {
-    path: __dirname + "/assets",
-    filename: "bundle.js"
+    path: __dirname + '/assets',
+    filename: 'bundle.js'
   },
 
   module: {
-    loaders: [
-      {
-        test: /\.json$/,
-        loader: "json"
-      },
+    rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: 'babel'
+        use: 'babel-loader'
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css?modules!postcss')
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            query: {
+              modules: true
+            }
+          },
+        })
       },
       {
         test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false&progressive=true'
+        use: [
+          'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false&progressive=true'
         ]
       },
       {
-        test: /\.woff$/, 
-        loader: "file-loader"
+        test: /\.woff$/,
+        use: 'file-loader'
       },
       {
         test: /\.mp3$/,
-        loader: 'file'
+        use: 'file-loader'
       }
     ]
   },
 
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
-      mangle: true, sourceMap: false,
+      compress: {
+        warnings: false
+      }
     }),
-    new webpack.optimize.DedupePlugin(),
-    new ExtractTextPlugin("style.css"),
+    new ExtractTextPlugin('style.css'),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
       }
-    })
+    }),
   ],
 
   resolve: {
