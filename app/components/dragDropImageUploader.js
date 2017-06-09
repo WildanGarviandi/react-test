8ea81acc6ee074230bfa9ecc855c30ad1b087cd9
Michael Theodore Pangestu <michael@Michaels-MacBook-Pro.local>
2017-06-09 12:38:12 +0700

    refactor

--- app/components/dragDropImageUploader.js
+++ app/components/dragDropImageUploader.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'; //eslint-disable-line
 import { connect } from 'react-redux';
 import Dropzone from 'react-dropzone';
+import PropTypes from 'prop-types';
 
-import ImagePreview from '../views/base/imagePreview';
 import FetchPost from '../modules/fetch/post';
 import styles from './dragDropImageUploader.css';
 
@@ -16,24 +16,22 @@ class DragDropImageUploader extends Component {
 
   uploadImage(acc) {
     if (acc) {
-      var formData = new FormData();
+      let formData = new FormData();
       formData.append('file', acc[0]);
-      this.setState({'isUploading': true});
-      FetchPost(`/upload/picture`, this.props.token, formData, false, true).then((res) => {
-        if (res.status == 200) {
-          return res.json();
-        }
-      }).then((data) => {
-        this.props.updateImageUrl(data.data.Location);
-        this.setState({'isUploading': false});
-      });
+      this.setState({ isUploading: true });
+      FetchPost('/upload/picture', this.props.token, formData, false, true)
+        .then(res => res.status === 200 && res.json())
+        .then((data) => {
+          this.props.updateImageUrl(data.data.Location);
+          this.setState({ isUploading: false });
+        });
     }
   }
 
   render() {
-    const {currentImageUrl} = this.props;
+    const { currentImageUrl } = this.props;
 
-    return(
+    return (
       <Dropzone
         className={styles.outerDropzone}
         accept="image/jpeg, image/png"
@@ -41,34 +39,46 @@ class DragDropImageUploader extends Component {
       >
         {({ isDragReject }) => {
           if (isDragReject) {
-            return "Some files will be rejected";
+            return 'Some files will be rejected';
           }
-          if( currentImageUrl ) {
+          if (currentImageUrl) {
             return (
               <div className={styles.dragDropFiller}>
                 <img className={styles.imgPreview} src={currentImageUrl} />
                 <button>Edit Image</button>
               </div>
-            )
-          } else {
-            return (
-              <div className={styles.dragDropFiller}>
-                <img src="/img/icon-add-image-drag-drop.png" />
-                Drag your image to this area or tap this button
-                below to choose from your folder
-                <button>Add Image</button>
-              </div>
             );
           }
+          return (
+            <div className={styles.dragDropFiller}>
+              <img src="/img/icon-add-image-drag-drop.png" />
+              Drag your image to this area or tap this button
+              below to choose from your folder
+              <button>Add Image</button>
+            </div>
+          );
         }}
       </Dropzone>
-    )
+    );
   }
 }
 
+/* eslint-disable */
+DragDropImageUploader.propTypes = {
+  token: PropTypes.any,
+  updateImageUrl: PropTypes.func.isRequired,
+  currentImageUrl: PropTypes.any,
+};
+/* eslint-enable */
+
+DragDropImageUploader.defaultProps = {
+  token: null,
+  currentImageUrl: null,
+};
+
 function getToken(store) {
-  const {userLogged} = store.app;
-  const {token} = userLogged;
+  const { userLogged } = store.app;
+  const { token } = userLogged;
 
   return {
     token,
