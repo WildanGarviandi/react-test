a3b4fc919b3c9f186c6bba6a5732d248b7002e1d
Michael Theodore Pangestu <michael@Michaels-MacBook-Pro.local>
2017-07-19 12:04:26 +0700

    fix bodyComponent because no param assign

--- app/inbound/inboundOrdersBody.jsx
+++ app/inbound/inboundOrdersBody.jsx
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import * as _ from 'lodash';
 import moment from 'moment';
+import PropTypes from 'prop-types';
 
 import { Body } from '../views/base/table';
 import { conf, inboundOrdersColumns } from './inboundOrdersColumns';
@@ -77,7 +78,7 @@ Interval.defaultProps = {
   down: false,
 };
 
-function BodyComponent(type, keyword, item) {
+function BodyComponent(type, keyword, item, deadlineDiff) {
   switch (type) {
     case 'String': {
       return <TextCell text={item[keyword]} />;
@@ -95,13 +96,12 @@ function BodyComponent(type, keyword, item) {
       switch (keyword) {
         case 'Deadline': {
           const deadline = moment(new Date(item.CutOffTime)).add(config.inboundDeadlineFromCutOffTime, 'hour');
-
           return (
             <span>
-              {item[keyword] >= 0 &&
-                <Interval startTime={item[keyword]} down />
+              {deadlineDiff >= 0 &&
+                <Interval startTime={deadlineDiff} down />
               }
-              {item[keyword] < 0 &&
+              {deadlineDiff < 0 &&
                 <TextCell text={deadline.fromNow()} />
               }
             </span>
@@ -130,7 +130,7 @@ function InboundOrdersBody({ items }) {
     const deadlineDiff = deadline.diff(now);
 
     const cells = _.map(body, (column) => {
-      const cell = BodyComponent(column.type, column.keyword, item, idx);
+      const cell = BodyComponent(column.type, column.keyword, item, deadlineDiff);
       const className = `${styles.td} ${styles[column.keyword]}
        ${(deadlineDiff < 0 && column.keyword === 'Deadline') ? inboundStyles.redCell : ''}`;
 
