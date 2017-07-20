5863538acf71ff143fe0646c49e1264c4a4c95e3
Michael Theodore Pangestu <michael@Michaels-MacBook-Pro.local>
2017-07-20 13:25:04 +0700

    refactor grouping done

--- app/grouping/grouping.jsx
+++ app/grouping/grouping.jsx
@@ -1,108 +1,115 @@
-import lodash from 'lodash';
-import React from 'react';
-import {connect} from 'react-redux';
-import {push} from 'react-router-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { ModalContainer, ModalDialog } from 'react-modal-dialog';
+
+import PropTypes from 'prop-types';
+
 import GroupingTable from './groupingTable';
 import styles from './styles.scss';
-import {ButtonWithLoading, Input, Page, InputWithDefault} from '../views/base';
+import { Input, Page } from '../views/base';
 import * as Grouping from './groupingService';
-import {ModalContainer, ModalDialog} from 'react-modal-dialog';
-import ReactDOM from 'react-dom';
 import ModalActions from '../modules/modals/actions';
 
-const CreateTripModal = React.createClass({
-  getInitialState() {
-    return {
+class CreateTripModal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       addOrderQuery: '',
       orders: this.props.addedOrders || [],
       isSuccessCreating: false,
-    }
-  },
-  stateChange(key) {
-    return (value) => {
-      this.setState({[key]: value});
     };
-  },
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.createTrip = this.createTrip.bind(this);
+    this.addOrder = this.addOrder.bind(this);
+  }
   componentDidMount() {
     document.getElementById('addOrder') && document.getElementById('addOrder').focus();
-  },
+  }
   componentWillReceiveProps(nextProps) {
-    if (!nextProps['isDuplicate'] && !nextProps['isSuccessCreating']) {
+    if (!nextProps.isDuplicate && !nextProps.isSuccessCreating) {
       document.getElementById('addOrder') && document.getElementById('addOrder').focus();
     }
 
     this.setState({
       orders: nextProps.addedOrders,
-      isSuccessCreating: nextProps['isSuccessCreating']
+      isSuccessCreating: nextProps.isSuccessCreating,
     });
-  },
+  }
   componentDidUpdate() {
-    this.state.isSuccessCreating && document.getElementById('gotItButton') && document.getElementById('gotItButton').focus();
-  },
+    if (this.state.isSuccessCreating) {
+      document.getElementById('gotItButton') && document.getElementById('gotItButton').focus();
+    }
+  }
+  componentWillUnmount() {
+    document.getElementById('prepareBtn') && document.getElementById('prepareBtn').focus();
+  }
+  handleInputChange(key) {
+    return (value) => {
+      this.setState({ [key]: value });
+    };
+  }
   closeModal() {
     const thisClass = this;
-    function close () {
+    function close() {
       thisClass.setState({
         trip: {},
-        orders: []
+        orders: [],
       });
       thisClass.props.onClose();
     }
     if (this.state.orders.length > 0) {
       this.props.askClose({
         message: 'Are you sure to close? Your changes will be lost.',
-        action: function () {
+        action: () => {
           close();
         },
-        backElementFocusID: 'addOrder'
+        backElementFocusID: 'addOrder',
       });
     } else {
       close();
     }
-  },
+  }
   createTrip() {
     this.props.createTrip();
-  },
-  confirmSuccess() {
+  }
+  confirmSuccess() { // potential not used
     this.closeModal();
-  },
+  }
   addOrder() {
-    if (this.state.addOrderQuery === "") {
+    if (this.state.addOrderQuery === '') {
       return;
     }
-    this.setState({addOrderQueryCopy: this.state.addOrderQuery});
-    this.setState({addOrderQuery: ''});
+    this.setState({ addOrderQueryCopy: this.state.addOrderQuery });
+    this.setState({ addOrderQuery: '' });
     this.props.addOrder(this.state.addOrderQuery, 'addOrder');
-  },
+  }
   chooseOrder(val) {
     this.props.addOrder(val, 'addOrder');
-  },
-  componentWillUnmount() {
-    document.getElementById("prepareBtn") && document.getElementById("prepareBtn").focus();
-  },
+  }
   render() {
-    const {createdTrip, isDuplicate, duplicateOrders} = this.props;
+    const { isDuplicate, duplicateOrders } = this.props;
     const canCreate = this.state.orders.length > 0;
-    var totalWeight = 0;
-
-    const ordersContent = this.state.orders.map(function (order, index) {
+    let totalWeight = 0;
+    let duplicateOrdersContent = [];
+    const ordersContent = this.state.orders.map((order) => {
       totalWeight += order.PackageWeight;
       return (
-        <div className={styles.orderContent} key={index}>
+        <div className={styles.orderContent} key={order.UserOrderID}>
           <div className={styles.orderContentLeft}>
             <div className={styles.smallText}>To</div>
-            { order.DropoffAddress &&
+            {order.DropoffAddress &&
               <div>
-                <div className={styles.mediumText}>{order.DropoffAddress.FirstName + ' ' + order.DropoffAddress.LastName}</div>
+                <div className={styles.mediumText}>{`${order.DropoffAddress.FirstName} ${order.DropoffAddress.LastName}`}</div>
                 <div className={styles.smallText}>
-                  {order.DropoffAddress.Address1 + ', ' + order.DropoffAddress.Address2 + ', ' + order.DropoffAddress.City + ', ' +
-                    order.DropoffAddress.ZipCode}
+                  {`${order.DropoffAddress.Address1}, ${order.DropoffAddress.Address2}, ${order.DropoffAddress.City}, ${order.DropoffAddress.ZipCode}`}
                 </div>
               </div>
             }
           </div>
           <div className={styles.orderContentRight}>
-            <div style={{textAlign: 'center'}}>
+            <div style={{ textAlign: 'center' }}>
               <div className={styles.smallTextBold}>{order.UserOrderNumber}</div>
               <div className={styles.smallTextBold}>({order.WebOrderID})</div>
             </div>
@@ -111,25 +118,26 @@ const CreateTripModal = React.createClass({
       );
     });
 
-    var duplicateOrdersContent = [];
-
     if (isDuplicate) {
-      const thisComponent = this;
-      duplicateOrdersContent = duplicateOrders.map(function (order, index) {
+      duplicateOrdersContent = duplicateOrders.map((order) => {
         return (
-          <div className={styles.orderContent + ' ' + styles.orderContentHover} key={index} 
-            onClick={thisComponent.chooseOrder.bind(null, order.UserOrderNumber)}>
+          <div
+            role="none"
+            className={`${styles.orderContent} ${styles.orderContentHover}`}
+            key={order.UserOrderID}
+            onClick={() => this.chooseOrder(order.UserOrderNumber)}
+          >
             <div className={styles.orderContentLeft}>
               <div className={styles.smallText}>To</div>
-              { order.DropoffAddress &&
+              {order.DropoffAddress &&
                 <div>
-                  <div className={styles.bigText}>{order.DropoffAddress.FirstName + ' ' + order.DropoffAddress.LastName}</div>
+                  <div className={styles.bigText}>{`${order.DropoffAddress.FirstName} ${order.DropoffAddress.LastName}`}</div>
                   <div className={styles.mediumText}>{order.DropoffAddress.City}</div>
                 </div>
               }
             </div>
             <div className={styles.orderContentRight}>
-              <div style={{textAlign: 'center'}}>
+              <div style={{ textAlign: 'center' }}>
                 <div className={styles.smallTextBold}>{order.UserOrderNumber}</div>
                 <div className={styles.smallText}>({order.WebOrderID})</div>
               </div>
@@ -141,22 +149,25 @@ const CreateTripModal = React.createClass({
 
     return (
       <ModalDialog>
-        { isDuplicate &&
+        {isDuplicate &&
           <div className={styles.modal}>
             <div className={styles.modalHeader}>
               <h2 className={styles.modalTitle}>Please pick the right one</h2>
               <div className={styles.smallText}>
-                Order <strong>{this.state.addOrderQueryCopy}</strong> was found in more than one data</div>
+                Order
+                 <strong>{this.state.addOrderQueryCopy}</strong>
+                 was found in more than one data
+              </div>
             </div>
-            <div className={styles.modalContent1 + ' ' + styles.ordersContent}>
+            <div className={`${styles.modalContent1} ${styles.ordersContent}`}>
               {duplicateOrdersContent}
             </div>
           </div>
         }
-        { !this.state.isSuccessCreating && !isDuplicate &&
+        {!this.state.isSuccessCreating && !isDuplicate &&
           <div className={styles.modal}>
             <button className={styles.closeModalButton} onClick={this.closeModal}>
-              <img src="/img/icon-close.png" className={styles.closeButtonImage}/></button>
+              <img alt="close" src="/img/icon-close.png" className={styles.closeButtonImage} /></button>
             <div className={styles.modalHeader}>
               <h2 className={styles.modalTitle}>Add Orders to Bag</h2>
               <div className={styles.modalInfo}>
@@ -168,28 +179,35 @@ const CreateTripModal = React.createClass({
                   <div className={styles.smallText}>Total Weight (kg)</div>
                   <h2 className={styles.bigText}>{totalWeight}</h2>
                 </div>
-                <div style={{clear: 'both'}}></div>
+                <div style={{ clear: 'both' }} />
               </div>
             </div>
 
             <div className={styles.modalContent4}>
-              <Input id={'addOrder'} className={styles.input + ' ' + styles.addOrderInput} 
-                base={{value: this.state.addOrderQuery, placeholder: 'Write the EDS here to manually verify...'}}
-                type={'text'} onChange={this.stateChange('addOrderQuery')} onEnterKeyPressed={this.addOrder} />
+              <Input
+                id={'addOrder'}
+                className={`${styles.input} ${styles.addOrderInput}`}
+                base={{ value: this.state.addOrderQuery, placeholder: 'Write the EDS here to manually verify...' }}
+                type={'text'}
+                onChange={this.handleInputChange('addOrderQuery')}
+                onEnterKeyPressed={this.addOrder}
+              />
               <div className={styles.modalContentRight}>
-                <button className={styles.addOrderButton} onClick={this.addOrder} disabled={this.state.addOrderQuery === ""}>Add</button>
+                <button className={styles.addOrderButton} onClick={this.addOrder} disabled={this.state.addOrderQuery === ''}>Add</button>
               </div>
-              <div style={{clear: 'both'}}></div>
+              <div style={{ clear: 'both' }} />
             </div>
 
-            <div className={styles.modalContent1 + ' ' + styles.ordersContent}>
+            <div className={`${styles.modalContent1} ${styles.ordersContent}`}>
               {
                 (!canCreate) &&
                 <div className={styles.ordersContentEmpty}>
-                  <img src="/img/icon-scan-input.png" width="80" height="80"/>
+                  <img alt="scan input" src="/img/icon-scan-input.png" width="80" height="80" />
                   <div className={styles.mediumText}>Waiting for order input</div>
-                  <div className={styles.smallText}>Please scan the order that you want to put on this bag/trip 
-                    or do it manually by typing the order id on the input box above</div>
+                  <div className={styles.smallText}>
+                    Please scan the order that you want to put on this bag/trip
+                    or do it manually by typing the order id on the input box above
+                  </div>
                 </div>
               }
               {
@@ -199,35 +217,40 @@ const CreateTripModal = React.createClass({
                 </div>
               }
             </div>
-            
+
             <div className={styles.modalFooter}>
               <div className={styles.modalFooterLeft}>
                 <div className={styles.smallText}>
                   {
                     (!canCreate) &&
-                    <span>You have not filled this bag / trip with orders.<br/>
-                    Please add by scanning or do it manually by typing its order ID.</span>
+                    <span>You have not filled this bag / trip with orders.<br />
+                      Please add by scanning or do it manually by typing its order ID.</span>
                   }
                   {
                     (canCreate) &&
-                    <span>You have filled this bag / trip with orders.<br/>
-                    Please click on this button to continue.</span>
+                    <span>You have filled this bag / trip with orders.<br />
+                      Please click on this button to continue.</span>
                   }
                 </div>
               </div>
               <div className={styles.modalFooterRight}>
-                <button className={styles.createTripButton} onClick={this.createTrip}
-                  disabled={!canCreate}>Create Trip</button>
+                <button
+                  className={styles.createTripButton}
+                  onClick={this.createTrip}
+                  disabled={!canCreate}
+                >
+                  Create Trip
+                </button>
               </div>
             </div>
-          </div> 
-        }   
-        { this.state.isSuccessCreating &&
+          </div>
+        }
+        {this.state.isSuccessCreating &&
           <div className={styles.modal}>
             <div className={styles.modalHeader}>
               <h2 className={styles.modalTitle}>Success</h2>
-              <div className={styles.successContent + ' ' + styles.ordersContentEmpty}>
-                <img className={styles.successIcon} src={"/img/icon-success.png"} />
+              <div className={`${styles.successContent} ${styles.ordersContentEmpty}`}>
+                <img alt="success" className={styles.successIcon} src={'/img/icon-success.png'} />
                 <div className={styles.mediumText}>You have successfully created a new trip</div>
                 <div className={styles.smallText}>
                   This trip is ready to be dispatch, please dispatch it on the outbound page.
@@ -242,43 +265,71 @@ const CreateTripModal = React.createClass({
           </div>
         }
       </ModalDialog>
-    )
+    );
   }
-});
+}
 
-const GroupingPage = React.createClass({
-  getInitialState() {
-    return {
+/* eslint-disable */
+CreateTripModal.propTypes = {
+  addedOrders: PropTypes.array,
+  isDuplicate: PropTypes.bool,
+  isSuccessCreating: PropTypes.bool,
+  askClose: PropTypes.func,
+  createTrip: PropTypes.func,
+  addOrder: PropTypes.func,
+  duplicateOrders: PropTypes.array,
+  createdTrip: PropTypes.any,
+}
+/* eslint-enable */
+
+CreateTripModal.defaultProps = {
+  addedOrders: [],
+  isDuplicate: false,
+  isSuccessCreating: false,
+  askClose: () => {},
+  createTrip: () => {},
+  addOrder: () => {},
+  duplicateOrders: [],
+  createdTrip: {},
+};
+
+class GroupingPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       findOrderQuery: '',
-      showModal: false
-    }
-  },
+      showModal: false,
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
   componentDidMount() {
-    document.getElementById("prepareBtn").focus();
-  },
+    document.getElementById('prepareBtn').focus();
+  }
   componentWillReceiveProps(nextProps) {
-    if ((nextProps['isGrouping'])) {
+    if ((nextProps.isGrouping)) {
       this.openModal();
     }
     this.setState({
-      isSuccessEditing: nextProps['isSuccessEditing']
+      isSuccessEditing: nextProps.isSuccessEditing,
     });
-  },
+  }
   openModal() {
-    this.setState({showModal: true});
-  },
+    this.setState({ showModal: true });
+  }
   closeModal() {
-    this.setState({showModal: false});
-    this.props.doneCreateTrip()
-  },
+    this.setState({ showModal: false });
+    this.props.doneCreateTrip();
+  }
   render() {
-    const {isGrouping, total} = this.props;
+    const { total } = this.props;
     return (
-      <Page title="GROUPING" count={{itemName: 'Items', done: 'All Done', value: total}}>
+      <Page title="GROUPING" count={{ itemName: 'Items', done: 'All Done', value: total }}>
         {
           this.state.showModal &&
           <ModalContainer>
-            <CreateTripModal {...this.props} onClose={this.closeModal}/>
+            <CreateTripModal {...this.props} onClose={this.closeModal} />
           </ModalContainer>
         }
         <div className={styles.actionContainer}>
@@ -288,11 +339,36 @@ const GroupingPage = React.createClass({
       </Page>
     );
   }
-});
+}
 
-function mapStateToProps (state) {
-  const {grouping, orderDetails} = state.app;
-  const {total, isGrouping, trip, addedOrders, isSuccessCreating, createdTrip, duplicateOrders, isDuplicate} = grouping;
+/* eslint-disable */
+GroupingPage.propTypes = {
+  isGrouping: PropTypes.bool,
+  isSuccessEditing: PropTypes.bool,
+  doneCreateTrip: PropTypes.func,
+  total: PropTypes.number,
+}
+/* eslint-enable */
+
+GroupingPage.defaultProps = {
+  isGrouping: false,
+  isSuccessEditing: false,
+  doneCreateTrip: () => {},
+  total: 0,
+};
+
+function mapStateToProps(state) {
+  const { grouping } = state.app;
+  const {
+    total,
+    isGrouping,
+    trip,
+    addedOrders,
+    isSuccessCreating,
+    createdTrip,
+    duplicateOrders,
+    isDuplicate,
+  } = grouping;
 
   return {
     total,
@@ -302,31 +378,33 @@ function mapStateToProps (state) {
     addedOrders,
     createdTrip,
     duplicateOrders,
-    isDuplicate
-  }
+    isDuplicate,
+  };
 }
 
-function mapDispatchToProps (dispatch) {
-  return {
-    createTrip: function () {
+function mapDispatchToProps(dispatch) {
+  const dispatchFunc = {
+    createTrip: () => {
       dispatch(Grouping.CreateTrip());
     },
-    doneCreateTrip: function () {
+    doneCreateTrip: () => {
       dispatch(Grouping.DoneCreateTrip());
     },
-    addOrder: function (orderQuery, backElementFocusID) {
+    addOrder: (orderQuery, backElementFocusID) => {
       dispatch(Grouping.AddOrder(orderQuery, backElementFocusID));
     },
-    askRemove: function (modal) {
+    askRemove: (modal) => {
       dispatch(ModalActions.addConfirmation(modal));
     },
-    removeOrder: function (index) {
+    removeOrder: (index) => {
       dispatch(Grouping.RemoveOrder(index));
     },
-    askClose: function (modal) {
+    askClose: (modal) => {
       dispatch(ModalActions.addConfirmation(modal));
-    }
-  }
+    },
+  };
+
+  return dispatchFunc;
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(GroupingPage);
