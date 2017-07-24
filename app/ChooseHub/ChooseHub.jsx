344264cb9f896d8de700fbb86472f3186c30cc97
sujono91 <sujono@etobee.com>
2017-07-24 15:32:33 +0700

    TMSDASH-501: Add Choose Hub Header

--- app/ChooseHub/ChooseHub.jsx
+++ app/ChooseHub/ChooseHub.jsx
@@ -1,4 +1,6 @@
 import React, { PureComponent } from 'react';
+
+import configValues from '../config/configValues.json';
 import styles from './styles.scss';
 
 class ChooseHub extends PureComponent {
@@ -6,7 +8,22 @@ class ChooseHub extends PureComponent {
     return (
       <div className={styles.page}>
         <div className={styles.choose}>
-            123
+          <div className={styles.choose__header}>
+            <div className={styles['header-top']}>
+              <div className={styles['header-top__img']}>
+                <img
+                  src={configValues.IMAGES.LOGO}
+                  alt="etobee logo"
+                />
+              </div>
+              <div className={styles['header-top__title']}>
+                <span>etobee</span>
+              </div>
+            </div>
+            <div className={styles['choose__header-hub']}>
+              <span>Choose a hub</span>
+            </div>
+          </div>
         </div>
       </div>
     );
