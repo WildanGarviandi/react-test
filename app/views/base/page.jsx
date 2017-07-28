22d8c71e0ff5e7111612fe90c3480f15cc4b0993
sujono91 <sujono@etobee.com>
2017-07-28 14:34:01 +0700

    Add IS_ACTIVATE_GOOGLE_AUTH Validation

--- app/views/base/page.jsx
+++ app/views/base/page.jsx
@@ -4,6 +4,7 @@ import { browserHistory } from 'react-router';
 import Glyph from '../../components/Glyph';
 import { ButtonWithLoading } from './';
 import styles from './page.scss';
+import configValues from '../../config/configValues.json';
 import ProfileMenuContainer from '../../containers/ProfileMenuContainer';
 
 const PageTitle = ({ additional, title, backButton, count }) => {
@@ -58,7 +59,7 @@ const ClassifyChildren = (children) => {
   });
 
   return { body, buttons: MoveButtonToTopRight(buttons), backLink };
-}
+};
 
 export default React.createClass({
   render() {
@@ -70,7 +71,7 @@ export default React.createClass({
         {backLink}
         {buttons}
         <PageTitle title={title} additional={additional} backButton={backButton} count={count} />
-        <ProfileMenuContainer />
+        {configValues.IS_ACTIVATE_GOOGLE_AUTH && <ProfileMenuContainer />}
         {body}
       </div>
     );
